8310ffe97e0fb7469f42eaa657f0c6d4f5f1a59a
Egor123468 <egorkotov1905@gmail.com>
2024-09-18 18:21:47 +0500

    third commit

--- hookah.js
+++ hookah.js
@@ -1,2 +1,80 @@
 const collapseElementList = document.querySelectorAll('.collapse')
 const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: '2%',
+    centeredSlides: false,
+    autoHeight: true,
+    initialSlide: 0
+    // on: {
+    //     slideChange: function () {
+    //         if (window.innerWidth <= 768) {
+    //             const slides = this.slides;
+    //             slides.forEach((slide, index) => {
+    //                 if (index === this.activeIndex) {
+    //                     slide.style.opacity = '1';
+    //                 } else {
+    //                     slide.style.opacity = '0.4';
+    //                 }
+    //             });
+    //         }
+    //     },
+    // },
+});
+
+// // Инициализация прозрачности слайдов
+// swiper.on('init', function () {
+//     if (window.innerWidth <= 768) {
+//         const slides = this.slides;
+//         slides.forEach((slide, index) => {
+//             if (index === this.activeIndex) {
+//                 slide.style.opacity = '1';
+//             } else {
+//                 slide.style.opacity = '0.4';
+//             }
+//         });
+//     }
+// });
+
+// window.addEventListener('resize', function () {
+//             swiper.update();
+//             if (window.innerWidth <= 768) {
+//                 const slides = swiper.slides;
+//                 slides.forEach((slide, index) => {
+//                     if (index === swiper.activeIndex) {
+//                         slide.style.opacity = '1';
+//                     } else {
+//                         slide.style.opacity = '0.4';
+//                     }
+//                 });
+//             } else {
+//                 const slides = swiper.slides;
+//                 slides.forEach((slide) => {
+//                     slide.style.opacity = '1';
+//                 });
+//             }
+//         });
+
+        // Переинициализация слайдера при изменении размера окна
+        // window.addEventListener('resize', function () {
+        //     swiper.update();
+        //     if (window.innerWidth <= 768) {
+        //         const slides = swiper.slides;
+        //         slides.forEach((slide, index) => {
+        //             if (index === swiper.activeIndex) {
+        //                 slide.style.opacity = '1';
+        //             } else {
+        //                 slide.style.opacity = '0.4';
+        //             }
+        //         });
+        //     } else {
+        //         const slides = swiper.slides;
+        //         slides.forEach((slide) => {
+        //             slide.style.opacity = '1';
+        //         });
+        //     }
+        // });
+
+        
